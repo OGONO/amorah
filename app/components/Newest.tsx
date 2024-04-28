49ce9741b05d08e9ea4f8020bb187f9726de1924
OGONO <129948297+OGONO@users.noreply.github.com>
2024-04-28 15:14:24 +0300

    Update Newest.tsx

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+byimport Link from "next/link";
 import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
 import { ArrowRight } from "lucide-react";
@@ -39,16 +39,19 @@ export default async function Newest() {
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
+              
               <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Link href={`/product/${product.slug}`}>
+                
                 <Image
-                  
                   src={product.imageUrl}
                   alt="Product image"
                   className="w-full h-full object-cover object-center lg:h-full lg:w-full"
                   width={300}
                   height={300}
                 />
+                <Link href={`/product/${product.slug}`}>
+                      {product.name}
+                  </Link>
               </div>
 
               <div className="mt-4 flex justify-between">
